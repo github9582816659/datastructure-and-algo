152d5b63e92f5d271ea9ddf637cdefa089fde21a
choudhary2205rahul <choudhary2205rahul@gmail.com>
2021-07-10 08:24:30 +0530

    initial commit -2

---
+++ src/main/java/com/courses/crazzy/Data_Structures_Advance/Arrays/CountDistinctInArray.java
@@ -0,0 +1,15 @@
+package com.geekforgeeks.Data_Structures_Advance.Hashing;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CountDistinctInArray {
+
+    // Count Distinct in an Array : Create HashSet which remove duplicates
+    public int countDistinctInArray(int[] a) {
+        Set<Integer> hs = new HashSet<Integer>(IntStream.of(a).boxed().collect(Collectors.toList()));
+        return hs.size();
+    }
+}
